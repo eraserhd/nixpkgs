5fc7084707d5bc6d649be5cb2112efd1810e7831
Jason Felice <jason.m.felice@gmail.com>
2019-05-12 11:34:33 -0400

    kak-ansi: init at 0.2.0

--- pkgs/applications/editors/kakoune/plugins.nix
+++ pkgs/applications/editors/kakoune/plugins.nix
@@ -1,5 +1,42 @@
-{ parinfer-rust }:
+{ stdenv, parinfer-rust, fetchFromGitHub }:
 
 {
-    inherit parinfer-rust;
+  inherit parinfer-rust;
+
+  kak-ansi = stdenv.mkDerivation rec {
+    name = "kak-ansi";
+    version = "0.2.0";
+    src = fetchFromGitHub {
+      owner = "eraserhd";
+      repo = "kak-ansi";
+      rev = "v${version}";
+      sha256 = "15bw3rhwmary50j850akm766ig6klflgkily60m5zvic0rsvlk2p";
+    };
+
+    buildInputs = [ stdenv ];
+
+    buildPhase = ''
+      $CC -O2 -std=c99 -o kak-ansi-filter kak-ansi-filter.c
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin $out/share/kak/autoload/plugins/
+      cp kak-ansi-filter $out/bin/
+
+      # Hard-code path of filter and don't try to build when Kakoune boots
+      sed '
+        /^declare-option.* ansi_filter /i\
+declare-option -hidden str ansi_filter %{'"$out"'/bin/kak-ansi-filter}
+        /^declare-option.* ansi_filter /,/^}/d
+      ' rc/ansi.kak >$out/share/kak/autoload/plugins/ansi.kak
+    '';
+
+    meta = with stdenv.lib; {
+      description = "Kakoune support for rendering ANSI code.";
+      homepage = "https://github.com/eraserhd/kak-ansi";
+      license = licenses.publicDomain;
+      maintainers = with maintainers; [ eraserhd ];
+      platforms = platforms.all;
+    };
+  };
 }
